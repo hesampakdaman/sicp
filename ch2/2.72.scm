46245b9b3b72045c82602a23d8bab41feac35055
Hesam Pakdaman <hesam.pakdaman@gmail.com>
2021-06-05 13:11:31 +0200

    Ex. 2.72

--- ch2/2.72.scm
+++ ch2/2.72.scm
@@ -1,53 +1,66 @@
-;; TOD:
-;; * Fix so memq 2 times until k
-;; * 2 calls to memq
-;;
 ;; The Huffman trees produced by choosing the relative frequencies as
 ;; in Ex. 2.71 are of the following form
 ;;
 ;;
 ;;                                   n *
 ;;                                    / \
-;;                               n-1 *   *
+;;                               n-1 *   * 1
 ;;                                  / \
-;;                             n-2 *   *
+;;                             n-2 *   * 1
 ;;                                /
 ;;                               .
 ;;                              .
 ;;                             .
 ;;                          2 *
 ;;                           / \
-;;                        1 *   *
+;;                        1 *   * 1
 ;;
 ;;
-;; where the numbers represent the number of symbols associated with a
-;; node that has two branches. Un-numbered nodes are associated with
-;; exactly one symbol.
+;; where the numbers represent the amount of symbols associated with a
+;; node. A Huffman tree of the form above has for n symbols a height
+;; of n.
 ;;
-;; In worst case (encode-symbol) will have to visit traverse down the
-;; entire tree. A Huffman tree of the form above has for n symbols
-;; a height of n.
+;; For each node the procedure has to search for the input-symbol in
+;; its symbol-list. The start node is associated with all symbols. For
+;; an arbitrary node associated with k symbols, its right-branch is
+;; only associated with one symbol while the left-branch is associated
+;; with k-1 nodes. This is due to the relative frequencies (see Ex
+;; 2.71) and our implementation choice of putting nodes with smaller
+;; weights on the left-branch (Ex. 2.69).
 ;;
-;; For each step in the procedure, the procedure has to search for the
-;; symbol in the symbol-list associated with a node. The start node
-;; are associated with all symbols. For a arbitrary node associated
-;; with k symbols, its left-branch is only associated with one symbol
-;; while the right-branch is associated with k-1 nodes. This is due to
-;; the relative frequencies (see Ex 2.71).
+;; We use memq procedure to search for a symbol. In general memq has a
+;; linear time-complexity O(m) for a list of m elements.  To search
+;; for a symbol with relative frequency 2^{j+1} at a node that is
+;; associated with j+1 <= k symbols takes j steps. This is due to how
+;; the symbols are collected at each node. The symbols-list at a node
+;; is ordered ascending so that symbol with frequency 2^{j+1} is at
+;; the j:th place in the symbol-list. memq is actually used twice at
+;; each step, once for the root node and once again for the
+;; left-branch.
 ;;
 ;; Taking the above into account, the total time-complexity of
-;; (encode-symbol) to encode the symbol with relative frequency 2^k is
+;; (encode-symbol) to encode the k:th symbol (with relative frequency
+;; 2^{k+1}) is
 ;;
-;;              f(k) = sum[i=1 to k] 2*k = 2*k^2.
+;;                f(k) = sum[i=n-k to n] 2*k ~ k * (n-k+1)
 ;;
-;; The most frequent symbol is found at the top of the tree. It
-;; therefore takes only two steps to reach it (initial call and one
-;; additional recursive call).
 ;;
-;;                            f(1) = 2.
+;; The most frequent symbol is the n:th symbol with a frequency of 2^n-1
+;;
+;;                             f(n) = n
 ;;
 ;; The least frequent symbol is found at the bottom of the tree. We
-;; have to traverse down to the bottom and preform checks for the
-;; symbol at each step. The total number of steps is at most
+;; have to traverse down to down the whole tree and preform checks for
+;; the symbol at each step. The total number of steps is at most
 ;;
-;;                        f(n) = 2*n^2
+;;                            f(1) = n.
+;;
+;; BONUS: Had we implemented (make-code-tree) such that each
+;; symbol-list associated with the resulting node is sorted descending
+;; w.r.t. symbol frequency, then memq would take n-k+1 steps to search
+;; for the k:th symbol in the input-list. Hence
+;;
+;;                h(k) = sum[i=n-k to n] 2*(n-k+1) ~ (n-k+1)^2,
+;;
+;; which leads to h(n) = 1 and h(1) = n^2. But this comes at the price
+;; of increasing time-complexity for creating the entire Huffman-tree.
