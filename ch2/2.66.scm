56a6d10e561f50721019513217edf1579d930322
Hesam Pakdaman <hesam.pakdaman@gmail.com>
2024-02-03 20:38:12 +0100

    Fix ex 2.66

--- ch2/2.66.scm
+++ ch2/2.66.scm
@@ -2,7 +2,7 @@
 ;; binary-trees and modify it to operate on keys instead.
 (define (lookup given-key set-of-records)
   (cond ((null? set-of-records) false)
-	((= given-key (key (entry set-of-records))) true)
+	((= given-key (key (entry set-of-records))) (value (entry set-of-records)))
 	((< given-key (key (entry set-of-records)))
 	 (lookup given-key (left-branch set-of-records)))
 	((> given-key (key (entry set-of-records)))
